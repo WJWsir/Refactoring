9ef9e8ed5c68408b72cac57b67389efd2a75e59c
WJWsir <WJWsir@gmail.com>
2021-12-09 23:50:19 +0800

    initialize

---
+++ initialization.js
@@ -0,0 +1,12 @@
+/**
+ * 1. 配置 VS Code 的 Code Runner 插件，目标可以方便地运行 js 代码
+ * 2. 初始化当前目录为git repository，准备跟随 重构 一书第一个示例
+ * 3. 誊抄第一个示例，并可以使用 node 编译运行，考虑使用某个单元测试框架测试或自己编写测试用例，commit 首次
+ * 4. 记录 不解的问题与思考、书籍内容的概括性总结、心得体会
+ */
+
+let person = {};
+person.name = "WJWsir";
+person.gender = "male";
+person.age = "21";
+console.log(person);
