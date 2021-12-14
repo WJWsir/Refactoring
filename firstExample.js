8dc3d9563f31095daae0b246b61d4621a8046deb
WJWsir <1710660215@tiangong.edu.cn>
2021-12-14 10:29:09 +0800

    删除参数：移出play参数step4

--- firstExample.js
+++ firstExample.js
@@ -34,7 +34,7 @@ function statement(invoice, plays) {
 	}).format;
 	for (let perf of invoice.performances) {
 		
-		let thisAmount = amountFor(perf, playFor(perf));
+		let thisAmount = amountFor(perf);
 		// add volume credits
 		volumeCredits += Math.max(perf.audience - 30, 0);
 		// add extra credit for every ten comedy attendees
