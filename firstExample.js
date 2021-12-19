3b071251215e6e199a2299095123bc11c092d24d
WJWsir <WJWsir@gmail.com>
2021-12-19 19:28:12 +0800

    搬移计算总数的函数: totalAmount, totalVolumeCredits

--- firstExample.js
+++ firstExample.js
@@ -25,6 +25,8 @@ function statement(invoice, plays){
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+	statementData.totalAmount = totalAmount(statementData);
+	statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -73,6 +75,22 @@ function statement(invoice, plays){
         return result;
     }
 
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+
+        return result;
+    }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -86,12 +104,11 @@ function renderPlainText(data, plays) {
 			perf.audience
 		} seats)\n`;
 	}
-	result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
-	result += `You earned ${totalVolumeCredits()} credits\n`;
+	result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
+	result += `You earned ${data.totalVolumeCredits} credits\n`;
 	return result;
 
     function usd(aNumber){
-        
         return new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD",
@@ -99,20 +116,4 @@ function renderPlainText(data, plays) {
         }).format(aNumber);
     }
 
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-
-        return result;
-    }
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
 }
